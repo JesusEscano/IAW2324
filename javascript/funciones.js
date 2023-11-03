77dff20412a3c4486260fbb6aa1115a45197d97e
JesusEscano <jesusescano83@iesamachado.org>
2023-11-03 13:20:00 +0100

    dfsdf

--- javascript/funciones.js
+++ javascript/funciones.js
@@ -1,30 +1,30 @@
 
-    var resultado = 0
+var resultado = 0
     function sumar(){
         let x = parseFloat(document.getElementById('numero1').value);
         let y = parseFloat(document.getElementById('numero2').value);
-        resultado = x+y;
+        return x+y;
     }
     function restar(){
         let x = parseFloat(document.getElementById('numero1').value);
         let y = parseFloat(document.getElementById('numero2').value);
-        resultado = x-y;
+        return x-y;
     }
     function division(){
         let x = parseFloat(document.getElementById('numero1').value);
         let y = parseFloat(document.getElementById('numero2').value);
         if (y!=0){
-            resultado = x/y;
+            return x/y;
         }
-        else {resultado = 'No se puede dividir por cero'}
+        else {return 'No se puede dividir por cero'}
     }
     function multiplicar(){
         let x = parseFloat(document.getElementById('numero1').value);
         let y = parseFloat(document.getElementById('numero2').value);
-        resultado = x*y;
+        return x*y;
     }
     function potencia(){
         let x = parseFloat(document.getElementById('numero1').value);
         let y = parseFloat(document.getElementById('numero2').value);
-        resultado = Math.pow(x,y);
+        return Math.pow(x,y);
     }
