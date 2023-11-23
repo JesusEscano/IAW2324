fbc727c41b1f085ca7ae55873d66929f3ce68e92
[JesusEscano] <[jesusescano83@iesamachado.org]>
2023-11-23 17:46:24 +0100

    nduf

--- php/operadoresNumericos.php
+++ php/operadoresNumericos.php
@@ -7,7 +7,45 @@
 </head>
 <body>
     <?php
+        // Definición de variables
+        $a = 5;
+        $b = 3;
+
+        // Suma
+        $suma = $a + $b;
+        echo "<p>Suma: $a + $b = $suma</p>";
     
+        // Resta
+        $resta = $a - $b;
+        echo "<p>Resta: $a - $b = $resta</p>";
+    
+        // Multiplicación
+        $multi = $a * $b;
+        echo "<p>Multiplicación: $a * $b = $multi</p>";
+    
+        // División
+        $division = $a / $b;
+        echo "<p>División: $a / $b = $division</p>";
+    
+        // Resto
+        $resto = $a % $b;
+        echo "<p>Resto de la división: $a % $b = $resto</p>";
+    
+        // Incremento
+        $a++;
+        echo "<p>El siguiente número de 5 es $a</p>";
+    
+        // Decremento
+        $b--;
+        echo "<p>El número anterior a 3 es $b</p>";
+
+        // POTEEEEEENCIA
+        $potencia = $a ** $b;
+        echo "</p>El resultado de elevar $a a $b es $potencia</p>";
+
+        //Raíz
+        $raiz = pow($a, 1/$b);
+        echo "<p>El resultado de hacer la raíz $b (cuadrada) de $a es $raiz</p>";
     ?>
 </body>
 </html>
