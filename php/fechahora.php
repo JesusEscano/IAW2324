5f35dcbc2e277e9aa52441dafd0952432c2f9621
JesusEscano <jesusescano83@iesamachado.org>
2023-11-29 10:42:45 +0100

    sdgsg

--- php/fechahora.php
+++ php/fechahora.php
@@ -3,13 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv='refresh' content='60'>
     <title>Fecha y hora</title>
 </head>
 <body>
     <h1>Fecha y hora actual</h1>
 <?php
 setlocale(LC_TIME, 'es_ES.UTF-8');
-$hechayhora = strftime('%A, %d de %B de %Y %H:%M:%S');
+$hechayhora = strftime('%A, %d de %B del año %Y y la hora es: %H:%M:%S');
 echo "<p>$hechayhora</p>";
 ?>
 </body>
