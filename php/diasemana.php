e3733f5c79f083f8332f6904467c720843caaa2f
JesusEscano <jesusescano83@iesamachado.org>
2023-11-29 12:51:21 +0100

    fgdg

--- php/diasemana.php
+++ php/diasemana.php
@@ -10,29 +10,22 @@
 $dia = date('N');
 
     switch ($dia) {
-        case 1:
-        echo "<p>Hoy en lunes, comienza la semana.</p>";
-        break;
-        case 2:
-        echo "<p>Orale putos, ya es martes. ¡¡¡¡¡AAAAAAAAAAAAAAAAAAAAAAAAA!!!!!</p>";
-        break;
-        case 3:
-        echo "<p>Miércoles, siempre en medio.</p>";
-        break;
-        case 4:
-        echo "<p>¿Hoy es jueves? ¡Ay la madre que me parió!</p>";
-        break;
-        case 5:
-        echo "<p>Viernes, un poco más y a descansar.</p>";
-        break;
-        case 6:
-        echo "<p>Sábado, sabadete. La rima se hace sola.</p>";
-        break;
-        case 7:
-        echo "<p>Domingo... espero que esté todo listo para el lunes</p>";
-        break;
-        default:
-        echo "<p>Esto sale si no carga los días por alguna razón. ¿Qué has roto, Jesús?</p>";
+        if (case = 1){
+        echo "<p>Hoy en lunes, comienza la semana.</p>";}
+        else if (case = 2){
+        echo "<p>Orale putos, ya es martes. ¡¡¡¡¡AAAAAAAAAAAAAAAAAAAAAAAAA!!!!!</p>";}
+        else if (case = 3){
+        echo "<p>Miércoles, siempre en medio.</p>";}
+        else if (case = 4){
+        echo "<p>¿Hoy es jueves? ¡Ay la madre que me parió!</p>";}
+        else if (case = 5){
+        echo "<p>Viernes, un poco más y a descansar.</p>";}
+        else if (case = 6){
+        echo "<p>Sábado, sabadete. La rima se hace sola.</p>";}
+        else if (case = 7){
+        echo "<p>Domingo... espero que esté todo listo para el lunes</p>";}
+        else {
+        echo "<p>Esto sale si no carga los días por alguna razón. ¿Qué has roto, Jesús?</p>";}
 }
 ?>
 </body>
