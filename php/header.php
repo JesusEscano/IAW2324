8f7f6a0080f59db71c2e327e44afdf4da0b05c35
JesusEscano <jesusescano83@iesamachado.org>
2023-12-11 13:56:42 +0100

    dsfsf

--- php/header.php
+++ php/header.php
@@ -3,6 +3,8 @@ header("refresh:3;url=https://myblog-akoycdevr3.live-website.com/php/saludo.php"
 // Esto vale para que podamos incluir redirecciones con una pantalla de carga 
 // para comprobar ciertos elementos antes de permitir seguir, por ejemplo, hay 
 // de Cloudflare contra DDoS. También vale para evitar mostrar errores de búsqueda.
+// O evitar que puedan copiar nuestro enlace con sesión iniciada y abrirlo en
+// otro navegador sin tener que iniciar sesión.
 ?>
 <!DOCTYPE html>
 <html lang="es">
