171f26a081fc9c8af7dbcc5836befaad7f31c641
JesusEscano <jesusescano83@iesamachado.org>
2024-01-10 12:55:33 +0100

    dfsfds

--- mysqlproyecto/db.php
+++ mysqlproyecto/db.php
@@ -1,8 +1,8 @@
 <?php
-$host = 'database-5014818324.webspace-host.com';   
-$user = 'dbu2458655';   
-$pass = "Adell83IES";   
-$database = 'dbs12312372';     
+$host = 'sql305.thsite.top';   
+$user = 'thsi_35757315';   
+$pass = "3bdDUYsw";   
+$database = 'thsi_35757315_incidencias';     
 $conn = mysqli_connect($host,$user,$pass,$database);   
 if (!$conn) {                                             
     die("Conexión fallida con base de datos: " . mysqli_connect_error());     
--- mysqlproyecto/footer.php
+++ mysqlproyecto/footer.php
@@ -1,3 +1,3 @@
-<footer class="blockquote-footer fixed-bottom">Gestión de incidencias del <a href="https://iesamachado.org" target="_blank">IES Antonio Machado</a>. Desarrollado por Jose Luis Núñez</footer>
+<footer class="blockquote-footer fixed-bottom">Gestión de incidencias del <a href="https://iesamachado.org" target="_blank">IES Antonio Machado</a>. Desarrollado por Jesús Escaño Moreno</footer>
 </body>
 </html>
