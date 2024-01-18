c2b2bf696c85a9db0e20d10a58bdc1cf742e45ae
[JesusEscano] <[jesusescano83@iesamachado.org]>
2024-01-18 18:06:07 +0100

    sdfs

--- mysqlproyecto/login.php
+++ mysqlproyecto/login.php
@@ -1,11 +1,28 @@
 <?php include "header.php" ?>
-<div class="container mt-5">
+<section>
+<div class="container mt-5 pt-5">
+    <div class="row">
+    <div class="col-12 col-sm-8 col-md-6 m-auto">
+    <div class="card border-0 shadow">
+    <div class="card-body">
+    <img src="/IESlogo.png" alt="IES Antonio Machado">
+    <svg style="display: block; margin: 0 auto;" xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+  <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
+  <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/>
+     </svg>
 <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>" method="post">
-<p class="text-center"><input type="text" id="nombre" name="nombre" placeholder="Usuario" required></p>
-<p class="text-center"><input type="password" id="contraseña" name="contraseña" placeholder="Contraseña" required></p>
-<p class="text-center"><input type="submit" class="btn btn-primary mt-2" name="submit" value="Iniciar sesión"></p>
+    <input type="text" id="nombre" name="nombre" placeholder="Usuario" class="form-control my-4 py-2" required></p>
+    <input type="password" id="contraseña" name="contraseña" placeholder="Contraseña" class="form-control my-4 py-2" required></p>
+    <div class="text-center mt-3">
+    <input type="submit" class="btn btn-primary mt-2" name="submit" value="Iniciar sesión"></p>
 </form>
-  </div>
+</div>
+</div> 
+</div>
+</div>
+</div>
+</div>
+</section>
   <?php
 if ($_POST){
     $usuario = htmlspecialchars($_POST["nombre"]);
@@ -37,4 +54,4 @@ if ($_POST){
     }   
 }
 ?>
-<?php include "footer.php" ?>
+<div class="text-center mt-3"><?php include "footer.php" ?></div>
